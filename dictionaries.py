39ae6a8be044d5d7a14df82b3501d6a51d6d310f
kbgodi <kbmabulana@gmail.com>
2025-07-09 16:13:06 +0200

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+#Operators
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Modules (%)
+# Exponent (**)
+x = 10
+y = 2
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(5%2)
+
+print(x**y)
+
+
+x = 10
+x = x+ 2
+
+x += 2
+print(x)
