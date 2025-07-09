d6a164f874b18978094a2e3d5de46eb5f0517490
kbgodi <kbmabulana@gmail.com>
2025-07-09 16:15:09 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#Loop Control Statements
+
+fruits = ["apple", "banana", "cherry", "date"]
+
+for fruit in fruits:
+    if fruit == "cherry":
+        break # Exits the loop if cherry is found 
+    
+    
+     
