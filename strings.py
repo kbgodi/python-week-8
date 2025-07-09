56e71f89477b6378e8283aa52e7ce2c6d2b8461b
kbgodi <kbmabulana@gmail.com>
2025-07-09 16:17:59 +0200

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,13 @@
+#Advanced concepts - Strings
+    
+message = ' Hello, World! '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # Convert all characters to lowercase
+print(message.split()) # spluit the string into a list based coma
+
+#upper method
+#replace method
+
+
+
